a94d36f91fabd22879ad9c783be18661fc3c3f4e
Dragana Petrovic <dragana.popov-petrovic@badin.rs>
2023-09-16 21:28:09 +0200

    fix api

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -59,7 +59,7 @@ const Customizer = () => {
       setGeneratingImg(true);
       
       const response = await fetch(
-        "http://localhost:8080/api.openai.com/v1/dalle",
+        "http://localhost:8080/api/v1/dalle",
         {
           method: "POST",
           headers: {
