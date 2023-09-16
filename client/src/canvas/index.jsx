ca5cdd128715ab6407806981e1826bc53e41c51e
Dragana Petrovic <dragana.popov-petrovic@badin.rs>
2023-09-17 01:29:44 +0200

    add environment

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,5 +1,5 @@
 import { Canvas } from "@react-three/fiber";
-import { Center } from "@react-three/drei";//Environment, 
+import { Environment, Center } from "@react-three/drei"; //
 
 import Shirt from "./Shirt";
 import Backdrop from "./Backdrop";
@@ -11,10 +11,10 @@ const CanvasModel = () => {
       shadows
       camera={{ position: [0, 0, 0], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
-      className="w-full max-w-full h-full transition-all ease-in"
+      className='w-full max-w-full h-full transition-all ease-in'
     >
       <ambientLight intensity={0.5} />
-      {/* <Environment preset="city" /> */}
+      <Environment preset='city' />
 
       <CameraRig>
         <Backdrop />
